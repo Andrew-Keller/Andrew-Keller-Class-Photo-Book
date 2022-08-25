e4a87e6d20258251b531dcb96a088c79ea3529c0
Andrew-Keller <112123995+Andrew-Keller@users.noreply.github.com>
2022-08-25 16:52:28 -0400

    indentations

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <!doctype html>
 <html lang="en">
 <head>
-<!-- https://www.bootdey.com/snippets/view/single-advisor-profile#html -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="assets/css/index.css" />
-<title>ASE 230 - class of Spring/Fall/Summer 20XX</title>
+	<!-- https://www.bootdey.com/snippets/view/single-advisor-profile#html -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="assets/css/index.css" />
+	<title>ASE 230 - class of Spring/Fall/Summer 20XX</title>
 </head>
 <body>
-<div class="container text-center">
-<h1>This is ASE 230 - class of Spring/Fall/Summer 20XX</h1>
-</div>
-<div class="container">
-<div class="row justify-content-center">
-<div class="col-12 col-sm-8 col-lg-6">
-<!-- Section Heading-->
-<div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-<h3>Our Creative <span> Team</span></h3>
-<p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
-<div class="line"></div>
-</div>
-</div>
-</div>
+	<div class="container text-center">
+		<h1>This is ASE 230 - class of Spring/Fall/Summer 20XX</h1>
+	</div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 col-sm-8 col-lg-6">
+				<!-- Section Heading-->
+					<div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+						<h3>Our Creative <span> Team</span></h3>
+							<p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+								<div class="line"></div>
+		</div>
+			</div>
+					</div>
 <div class="row">
-<!-- Single Advisor-->
-<div class="col-12 col-sm-6 col-lg-3">
-<div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-<!-- Team Thumb-->
-<div class="advisor_thumb"><a href="detail.html"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
+	<!-- Single Advisor-->
+		<div class="col-12 col-sm-6 col-lg-3">
+			<div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+				<!-- Team Thumb-->
+					<div class="advisor_thumb"><a href="detail.html"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a>
 <!-- Social Info-->
 <div class="social-info"><a href="detail.html"><i class="fa fa-facebook"></i></a><a href="detail.html"><i class="fa fa-twitter"></i></a><a href="detail.html"><i class="fa fa-linkedin"></i></a></div>
 </div>
